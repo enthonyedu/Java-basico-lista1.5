d8c260202ce5d1eb85a7c13d3d70c7a203397196
Matuk <mateusmatuk@fai-mg.br>
2022-03-07 20:38:47 -0300

    feat: v1 do ex 1

--- Java-basico-lista1.5-ex01/src/br/fai/ex01/Main.java
+++ Java-basico-lista1.5-ex01/src/br/fai/ex01/Main.java
@@ -1,10 +1,39 @@
 package br.fai.ex01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Main app = new Main();
+		app.start();
 
+	}
+	
+	private void start () {
+		Scanner scanner = new Scanner (System.in);
+		
+		int[] valores = new int[4];
+		
+		for (int i = 0; i < valores.length; i++) {
+			
+			System.out.println("Digite o valor "  + (i + 1) + ": ");
+			
+			valores[i] = scanner.nextInt();
+			
+		
+		}
+		
+		calculo(valores);
+		
+	}
+
+	private void calculo(int[] valores) {
+		
+		
+		// TODO Auto-generated method stub
 	}
 
 }
