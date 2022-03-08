162bbed48ba344ce8b8e49e82217e1624a15ee24
Enthony <enthonyedu@gmail.com>
2022-03-07 22:35:42 -0300

    feat: projeto do ex05 finalizado

--- Java-basico-lista1.5-ex05/src/br/fai/ex05/Main.java
+++ Java-basico-lista1.5-ex05/src/br/fai/ex05/Main.java
@@ -1,9 +1,34 @@
 package br.fai.ex05;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Frase: ");
+
+		String valor = s.nextLine();
+
+		if (valor.isEmpty()) {
+			System.out.println("Nada foi digitado");
+		} else {
+			String[] palavras = valor.split(" ");
+			System.out.println("Penultima palavra: " + palavras[palavras.length - 2]);
+			
+			if(palavras.length > 4) {
+				System.out.println("Texto muito longo");
+			}
+		}
 
 	}
 
