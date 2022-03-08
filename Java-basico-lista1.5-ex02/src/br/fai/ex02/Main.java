603feb6649cfabeb75b6dc35b1cc3b7d55af3b8c
Enthony <enthonyedu@gmail.com>
2022-03-07 21:52:46 -0300

    feat: projeto de ex02 criado

---
+++ Java-basico-lista1.5-ex02/src/br/fai/ex02/Main.java
@@ -0,0 +1,17 @@
+package br.fai.ex02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+
+	}
+
+}
