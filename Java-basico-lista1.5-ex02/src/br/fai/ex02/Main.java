58e810c6d04fbcd819a712bdf494c92ef1f2f646
Enthony <enthonyedu@gmail.com>
2022-03-07 22:05:26 -0300

    feat: ex02 finalizado

--- Java-basico-lista1.5-ex02/src/br/fai/ex02/Main.java
+++ Java-basico-lista1.5-ex02/src/br/fai/ex02/Main.java
@@ -1,5 +1,7 @@
 package br.fai.ex02;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,8 +12,43 @@ public class Main {
 
 	}
 
+	Scanner s = new Scanner(System.in);
+
 	private void start() {
 
+		double soma = 0.0;
+
+		int i = 0;
+		boolean emExecucao = true;
+		while (emExecucao) {
+
+			System.out.print("Valor " + (i + 1) + ": ");
+
+			Double valor = s.nextDouble();
+
+			System.out.println();
+
+			if (i >= 4) {
+				if (valor == -1) {
+					emExecucao = false;
+				}
+
+			}
+
+			if (emExecucao) {
+				soma += valor;
+				i += 1;
+			}
+		}
+		
+		System.out.println("Soma: " + soma);
+		calcular(soma, i);
+
+	}
+
+	private void calcular(Double soma, int i) {
+		double media = soma / i;
+		System.out.println("Média: " + media);
 	}
 
 }
