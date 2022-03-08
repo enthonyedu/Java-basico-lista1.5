246a7055db0ec1c3599c0d50abcc3a079c9d3965
Enthony <enthonyedu@gmail.com>
2022-03-07 21:51:27 -0300

    feat: Ex 01 concluido

--- Java-basico-lista1.5-ex01/src/br/fai/ex01/Main.java
+++ Java-basico-lista1.5-ex01/src/br/fai/ex01/Main.java
@@ -6,34 +6,89 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Main app = new Main();
 		app.start();
 
 	}
-	
-	private void start () {
-		Scanner scanner = new Scanner (System.in);
-		
+
+	private Scanner scanner = new Scanner(System.in);
+
+	private void start() {
+
 		int[] valores = new int[4];
-		
-		for (int i = 0; i < valores.length; i++) {
-			
-			System.out.println("Digite o valor "  + (i + 1) + ": ");
-			
-			valores[i] = scanner.nextInt();
-			
-		
+
+		int i = 0;
+
+		while (i < 4) {
+
+			System.out.print("Digite o valor " + (i + 1) + ": ");
+
+			switch (i) {
+			case 0:
+
+				valores[0] = scanner.nextInt();
+
+				break;
+
+			case 1:
+				valores[1] = scanner.nextInt();
+
+				break;
+			case 2:
+				valores[2] = scanner.nextInt();
+
+				break;
+
+			case 3:
+
+				valores[3] = scanner.nextInt();
+
+				break;
+
+			default:
+				break;
+			}
+
+			i += 1;
+
 		}
-		
-		calculo(valores);
-		
+
+		calculos(valores);
+
 	}
 
-	private void calculo(int[] valores) {
-		
-		
-		// TODO Auto-generated method stub
+	private void calculos(int[] valores) {
+
+		int maior = -1;
+		int menor = 35345346;
+
+		for (int i : valores) {
+
+			maior = Math.max(i, maior);
+
+		}
+
+		for (int i : valores) {
+
+			menor = Math.min(i, menor);
+
+		}
+
+		System.out.println("O menor valor é: " + menor);
+		System.out.println("O maior valor é: " + maior);
+
+		Classifica(maior);
+
+	}
+
+	private void Classifica(int maior) {
+		if (maior > 50) {
+			System.out.println("Valor muito alto");
+
+		} else {
+			System.out.println("Valor normal");
+		}
 	}
 
 }
