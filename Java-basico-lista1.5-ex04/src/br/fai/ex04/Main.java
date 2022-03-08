a9264c117a5e651f886a8a7a1b32acc7b11641ab
Enthony <enthonyedu@gmail.com>
2022-03-07 22:18:20 -0300

    feat: projeto de ex04 criado

---
+++ Java-basico-lista1.5-ex04/src/br/fai/ex04/Main.java
@@ -0,0 +1,20 @@
+package br.fai.ex04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		Scanner s = new Scanner(System.in);
+
+	}
+
+}
