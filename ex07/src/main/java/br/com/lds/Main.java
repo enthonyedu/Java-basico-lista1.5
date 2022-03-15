33fc57778efde45e65c0b466b9389962ff15edbe
enthonyedu <enthonyedu@gmail.com>
2022-03-14 22:04:24 -0300

    feat: finalizado o projeto do ex 07

--- ex07/src/main/java/br/com/lds/Main.java
+++ ex07/src/main/java/br/com/lds/Main.java
@@ -2,6 +2,7 @@ package br.com.lds;
 
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Random;
 
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -15,10 +16,38 @@ public class Main {
 
 	}
 
+	private final int VALOR_MINIMO_DO_TAMANHO_DO_ARRAY = 2;
+	private final int VALOR_MAXIMO_DO_TAMANHO_DO_ARRAY = 5;
+
+	private final int VALOR_MINIMO_DO_CONTEUDO_DO_ARRAY = -100;
+	private final int VALOR_MAXIMO_DO_CONTEUDO_DO_ARRAY = 100;
+
+	private int[] gerarArrayComValoresAleatorios() {
+
+		int tamanhoDoArray = obterValorAleatorio(VALOR_MINIMO_DO_TAMANHO_DO_ARRAY, VALOR_MAXIMO_DO_TAMANHO_DO_ARRAY);
+
+		int[] arrayDinamico = new int[tamanhoDoArray];
+
+		for (int i = 0; i < arrayDinamico.length; i++) {
+			arrayDinamico[i] = obterValorAleatorio(VALOR_MINIMO_DO_CONTEUDO_DO_ARRAY, VALOR_MAXIMO_DO_CONTEUDO_DO_ARRAY);
+		}
+		
+		return arrayDinamico;
+
+	}
+
+	private int obterValorAleatorio(int minimo, int maximo) {
+
+		Random random = new Random();
+
+		return random.nextInt((maximo - minimo) + 1) + minimo;
+	}
+
 	private void start() {
 
-		int[] primeiroArray = { 50, -20, 0, 40 };
-		int[] segundoArray = { 5, -50, 10, 60 };
+//		int[] primeiroArray = { 50, -20, 0, 40 };
+		int[] primeiroArray = gerarArrayComValoresAleatorios();
+		int[] segundoArray = gerarArrayComValoresAleatorios();
 
 		exibirValores(primeiroArray);
 		exibirValores(segundoArray);
@@ -30,9 +59,8 @@ public class Main {
 		System.out.println("Soma: " + soma);
 
 		double media = mediaArray(soma, arraysCombinados.length);
-		
-		exibirMedia(media);
 
+		exibirMedia(media);
 
 	}
 
@@ -65,15 +93,14 @@ public class Main {
 	}
 
 	private double mediaArray(int soma, int length) {
-		return (double) soma/length;
+		return (double) soma / length;
 
-		
 	}
-	
+
 	private void exibirMedia(double media) {
-		
+
 		DecimalFormat decimalFormat = new DecimalFormat("#.##");
-		
+
 		System.out.println("Media: " + decimalFormat.format(media));
 	}
 
