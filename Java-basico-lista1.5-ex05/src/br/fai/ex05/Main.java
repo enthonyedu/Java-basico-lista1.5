82d6d244bab38b503e9b9b90aa19334e3bf88698
enthonyedu <enthonyedu@gmail.com>
2022-03-14 21:22:30 -0300

    feat: criado o projeto do ex 07

--- Java-basico-lista1.5-ex05/src/br/fai/ex05/Main.java
+++ Java-basico-lista1.5-ex05/src/br/fai/ex05/Main.java
@@ -14,22 +14,19 @@ public class Main {
 
 	private void start() {
 		Scanner s = new Scanner(System.in);
+			System.out.print("Frase: ");
 
-		System.out.print("Frase: ");
+			String valor = s.nextLine();
 
-		String valor = s.nextLine();
+			if (valor.isEmpty()) {
+				System.out.println("Nada foi digitado");
+			} else {
+				String[] palavras = valor.split(" ");
+				System.out.println("Penultima palavra: " + palavras[palavras.length - 2]);
 
-		if (valor.isEmpty()) {
-			System.out.println("Nada foi digitado");
-		} else {
-			String[] palavras = valor.split(" ");
-			System.out.println("Penultima palavra: " + palavras[palavras.length - 2]);
-
-			if (palavras.length > 4) {
-				System.out.println("Texto muito longo");
+				if (palavras.length > 4) {
+					System.out.println("Texto muito longo");
+				}
 			}
-		}
-
 	}
-
 }
